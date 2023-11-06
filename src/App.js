84b52ce6bc13eae752e5b5b807f5f9ec467c5960
zaidsultan10 <zaidsultan09@gmail.com>
2023-11-06 22:27:55 +0400

    use state done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const counterFunc = () => {
+    console.log('function ran ---')
+    return 0
+  }
+  const [count, setCount] = useState(counterFunc())
+  const [pureObj, setPureObj] = useState({counter : 0, color : 'blue'})
+  const handleClick = (operation) => {
+    // operation === '+' ? setCount(prevCount => prevCount + 1) : setCount(prevCount => prevCount - 1)
+    if(operation === '+'){
+      setPureObj(prevState => {
+        return {
+          ...pureObj, 
+          counter: prevState.counter + 1,
+          color: 'green'
+        }
+      })
+    } else {
+      setPureObj(prevState => {
+        return {
+          ...pureObj, 
+          counter: prevState.counter - 1,
+          color: 'red'
+        }
+      })
+    }
+  }
   return (
     <div className="App">
-      <p>lol</p>
+      <button onClick={() => handleClick('+')}>
+        {`+`}
+      </button>
+      <h2 style={{color:pureObj.color}}>{`${pureObj.counter} ${pureObj.color}`}</h2>
+      <button disabled={!pureObj.counter} onClick={() => handleClick('-')}>
+        {`-`}
+      </button>
     </div>
   );
 }
