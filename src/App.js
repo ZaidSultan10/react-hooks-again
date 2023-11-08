a25497e1ba1d3e61ae1728c8bb68623d4789644f
zaidsultan10 <zaidsultan09@gmail.com>
2023-11-08 23:36:34 +0400

    use effect part 1 done

--- src/App.js
+++ src/App.js
@@ -1,42 +1,77 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  const counterFunc = () => {
-    console.log('function ran ---')
-    return 0
-  }
-  const [count, setCount] = useState(counterFunc())
-  const [pureObj, setPureObj] = useState({counter : 0, color : 'blue'})
-  const handleClick = (operation) => {
-    // operation === '+' ? setCount(prevCount => prevCount + 1) : setCount(prevCount => prevCount - 1)
-    if(operation === '+'){
-      setPureObj(prevState => {
-        return {
-          ...pureObj, 
-          counter: prevState.counter + 1,
-          color: 'green'
-        }
+  // const counterFunc = () => {
+  //   console.log('function ran ---')
+  //   return 0
+  // }
+  // const [count, setCount] = useState(counterFunc())
+  // const [pureObj, setPureObj] = useState({counter : 0, color : 'blue'})
+  // const handleClick = (operation) => {
+  //   // operation === '+' ? setCount(prevCount => prevCount + 1) : setCount(prevCount => prevCount - 1)
+  //   if(operation === '+'){
+  //     setPureObj(prevState => {
+  //       return {
+  //         ...pureObj, 
+  //         counter: prevState.counter + 1,
+  //         color: 'green'
+  //       }
+  //     })
+  //   } else {
+  //     setPureObj(prevState => {
+  //       return {
+  //         ...pureObj, 
+  //         counter: prevState.counter - 1,
+  //         color: 'red'
+  //       }
+  //     })
+  //   }
+  // }
+
+  const [resourceType, setresourceType] = useState('posts')
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
+  const [data, setData] = useState(null)
+  
+
+  useEffect(() => {
+    try{
+      setLoading(true)
+      fetch(`https://jsonplaceholder.typicode.com/${resourceType}`).then(resp => {
+        return resp.json()
+      }).then(data => {
+        console.log('data -->',data)
+        setData(data)
+        setLoading(false)
       })
-    } else {
-      setPureObj(prevState => {
-        return {
-          ...pureObj, 
-          counter: prevState.counter - 1,
-          color: 'red'
-        }
-      })
+    }catch(err){
+      console.log('err ->',err)
+      setLoading(false)
+      setError(err.message)
     }
-  }
+  }, [resourceType])
   return (
     <div className="App">
-      <button onClick={() => handleClick('+')}>
+      {/* <button onClick={() => handleClick('+')}>
         {`+`}
       </button>
       <h2 style={{color:pureObj.color}}>{`${pureObj.counter} ${pureObj.color}`}</h2>
       <button disabled={!pureObj.counter} onClick={() => handleClick('-')}>
         {`-`}
-      </button>
+      </button> */}
+      <button onClick={() => setresourceType('posts')}>{`Posts`}</button>
+      <button onClick={() => setresourceType('users')}>{`Users`}</button>
+      <button onClick={() => setresourceType('comments')}>{`Comments`}</button>
+      {loading && (<h2 style={{color:'blue'}}>{loading}</h2>)}
+      {error && (<p style={{color:'red'}}>{error}</p>)}
+      <h3>{resourceType}</h3>
+      {data && data.length > 0 && data.map((item, i) => (
+        <div>
+          <p>{item?.body}</p>
+          <p>{item?.name}</p>
+        </div>
+      ))}
     </div>
   );
 }
